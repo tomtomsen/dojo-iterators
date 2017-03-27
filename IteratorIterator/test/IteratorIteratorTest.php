323a7a6e0d747d694b5e14fb065c320b23853004
tom tomsen <tom.tomsen@mailbox.org>
2017-03-27 21:00:17 +0200

    added test group

--- IteratorIterator/test/IteratorIteratorTest.php
+++ IteratorIterator/test/IteratorIteratorTest.php
@@ -5,6 +5,9 @@ namespace tomtomsen\Iterators;
 require __DIR__ . '/../src/IteratorIterator.php';
 require_once __DIR__ . '/BasicIteratorIteratorTest.php';
 
+/**
+ * @group IteratorIterator
+ */
 class IteratorIteratorTest extends BasicIteratorIteratorTest
 {
 	protected function getIterator(...$params) {
