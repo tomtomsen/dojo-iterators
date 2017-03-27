6b2d757b40f3b3dc242f85b078d0ef61b697f3be
tom tomsen <tom.tomsen@mailbox.org>
2017-03-27 20:58:48 +0200

    basic implementation of FilterIterator

--- FilterIterator/test/FilterIteratorTest.php
+++ FilterIterator/test/FilterIteratorTest.php
@@ -5,7 +5,20 @@ namespace tomtomsen\Iterators;
 require __DIR__ . '/../src/FilterIterator.php';
 require_once __DIR__ . '/BaseFilterIteratorTest.php';
 
+/**
+ * @group FilterIterator
+ */
 class FilterIteratorTest extends BaseFilterIteratorTest
 {
+	protected function getIterator(...$params) {
+		return new FilterIteratorImpl(...$params);
+	}
+}
 
+class FilterIteratorImpl extends FilterIterator {
+	public function accept() {
+		$value = $this->getInnerIterator()->current();
+
+		return 0 < $value;
+	}
 }
