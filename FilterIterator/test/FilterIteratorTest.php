384409b325631e2f750c207efd5332ad47532edc
tom tomsen <tom.tomsen@inbox.com>
2017-03-27 20:30:25 +0200

    added base FilterIterator files

---
+++ FilterIterator/test/FilterIteratorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace tomtomsen\Iterators;
+
+require __DIR__ . '/../src/FilterIterator.php';
+require_once __DIR__ . '/BaseFilterIteratorTest.php';
+
+class FilterIteratorTest extends BaseFilterIteratorTest
+{
+
+}
